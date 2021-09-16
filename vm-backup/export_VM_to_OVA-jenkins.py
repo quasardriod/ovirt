b71888fd9e22d21aa880ad137e6b07d2ff87cd96
Boso <boso@localhost>
2021-09-16 22:05:49 +0530

    enabled supported for comma separated list

--- vm-backup/export_VM_to_OVA-jenkins.py
+++ vm-backup/export_VM_to_OVA-jenkins.py
@@ -21,7 +21,7 @@ def ovirt_vm_export():
     ovirt_user = os.environ['ovirt_user']
     ovirt_passwd = os.environ['ovirt_password']
     ca_cert = os.environ['ca_cert']
-    ovirt_host = os.environ['ovirt_hosts']
+    ovirt_host = os.environ['ovirt_host']
     exported_vms_volume = os.environ['exported_vms_volume']
     
     if not os.path.exists(ca_cert):
@@ -30,9 +30,13 @@ def ovirt_vm_export():
     # Ovirt host specific vars
     host_search_str = 'name' + '=' + ovirt_host
 
-    print("\n Backup will be triggered for following vms: {}".format(target_vm))
+    print("\n Backup will be triggered for following vms:")
+    for _vm in target_vm.split(','):
+        print(_vm)
+    
     print("\n Target Ovirt host: {}".format(ovirt_host))  
     
+    sys.exit("exit 0")
     # This example shows how to export a virtual machine as a Virtual
     # Appliance (OVA) file to a specified path on a host.
     # Create the connection to the server:
@@ -51,25 +55,24 @@ def ovirt_vm_export():
 
     # Find the virtual machine:
     vms_service = connection.system_service().vms_service()
-    for read_vm in target_vm:
-        vm_search_str = 'name' + '=' + read_vm
+    
+    for _vm in target_vm.split(','):
+        vm_search_str = 'name' + '=' + _vm
         vm_info = vms_service.list(search=vm_search_str)[0]
         vm_exported_file = vm_info.name + ".ova"
         print("\n VM info: \n href: {} \n ID: {} \n Name: {}".format(vm_info._href, vm_info._id, vm_info._name))
         vm_task = vms_service.vm_service(vm_info.id)
 
-    
-    # Export the virtual machine. Note that the 'filename' parameter is
-    # optional, and only required if you want to specify a name for the
-    # generated OVA file that is different from <vm_name>.ova.
-    # Note that this operation is only available since version 4.2 of
-    # the engine and since version 4.2 of the SDK.
-    vm_service.export_to_path_on_host(
-        host=types.Host(id=host.id),
-        directory=exported_vms_volume,
-        filename=vm_exported_file
-    )
-
+        # Export the virtual machine. Note that the 'filename' parameter is
+        # optional, and only required if you want to specify a name for the
+        # generated OVA file that is different from <vm_name>.ova.
+        # Note that this operation is only available since version 4.2 of
+        # the engine and since version 4.2 of the SDK.
+        vm_task.export_to_path_on_host(
+            host=types.Host(id=host.id),
+            directory=exported_vms_volume,
+            filename=vm_exported_file
+        )
     # Close the connection to the server:
     connection.close()
 ovirt_vm_export()
